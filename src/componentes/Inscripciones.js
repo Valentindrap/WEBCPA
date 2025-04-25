a2cef7d94b41258d05abf33b883f6cd342f69e91
Valentindrap <valentindrap01@gmail.com>
2025-04-25 14:35:57 -0300

    Metodos de pago

--- src/componentes/Inscripciones.js
+++ src/componentes/Inscripciones.js
@@ -6,23 +6,65 @@ function Inscripciones() {
     <div className="inscripciones">
       <h1>Inscripciones</h1>
 
+      <p>
+        En este espacio encontrarás toda la información necesaria para la inscripción al I COPAT-AL. 
+        Es importante saber que para poder inscribirse al Congreso deberán contar con el certificado 
+        de estudiante regular en caso de ser estudiante universitario de pregrado o grado; o el 
+        comprobante de pago en caso de tratarse de estudiantes universitarios de posgrado, Docentes 
+        y/o Investigadores y público en general.
+      </p>
+
       <h2>Aranceles</h2>
       <ul>
         <li>
-          <strong>Estudiantes universitarios (pregrado-grado):</strong> Inscripción libre y gratuita.
+          <strong>Estudiantes universitarios (pregrado-grado):</strong> inscripción libre y gratuita. Solo tienen que adjuntar el certificado de estudiante regular.
         </li>
-        <br></br>
+        <br />
         <li>
           <strong>Estudiantes universitarios de posgrado / Docentes y/o Investigadores:</strong> $40.000 hasta el mes de junio y $60.000 del mes de junio en adelante.
         </li>
-        <br></br>
+        <br />
         <li>
           <strong>Público general:</strong> $50.000 hasta el mes de junio y $70.000 del mes de junio en adelante.
         </li>
       </ul>
 
+      <h2>Medios de Pago</h2>
+      <p><strong>Opción 1:</strong> Transferencia o depósito bancario a la siguiente cuenta:</p>
+      <ul>
+        <li><strong>Número de Cuenta:</strong> CC en Pesos 153-012563/9</li>
+        <li><strong>CBU:</strong> 0720153620000001256390</li>
+        <li><strong>Alias:</strong> MACETA.FORO.FAROL</li>
+        <li><strong>Razón Social:</strong> UNIVERSIDAD NACIONAL DE LA PAM</li>
+        <li><strong>CUIT/CUIL:</strong> 30586762261</li>
+      </ul>
 
+      <p><strong>Opción 2:</strong> Link de pago con tarjetas de crédito o débito</p>
+      <ul>
+        <li>
+          <strong>Estudiantes universitarios de posgrado / Docentes y/o Investigadores:</strong><br />
+          Realizá el pago a UNIVERSIDAD NACIONAL DE LA PAM por $40.000,00 con el siguiente link:<br />
+          <a href="https://checkout.globalgetnet.com.ar/orders/ad8baa6f-c626-4700-8dbd-fbded464930f" target="_blank" rel="noopener noreferrer">
+            https://checkout.globalgetnet.com.ar/orders/ad8baa6f-c626-4700-8dbd-fbded464930f
+          </a>
+        </li>
+        <br />
+        <li>
+          <strong>Público en general:</strong><br />
+          Realizá el pago a UNIVERSIDAD NACIONAL DE LA PAM por $50.000,00 con el siguiente link:<br />
+          <a href="https://checkout.globalgetnet.com.ar/orders/ce24774e-c769-4df9-b96d-b9baa5ce7d9c" target="_blank" rel="noopener noreferrer">
+            https://checkout.globalgetnet.com.ar/orders/ce24774e-c769-4df9-b96d-b9baa5ce7d9c
+          </a>
+        </li>
+      </ul>
 
+      <h2>Formulario de Inscripción</h2>
+      <p>
+        Podés completar el formulario de inscripción desde el siguiente enlace:<br />
+        <a href="https://forms.gle/E7SuBMbPJQWwnGXd9" target="_blank" rel="noopener noreferrer">
+          https://forms.gle/E7SuBMbPJQWwnGXd9
+        </a>
+      </p>
     </div>
   );
 }
