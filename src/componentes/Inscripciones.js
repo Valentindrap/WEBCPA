4f8969a7c95f630ab91e22dbab06a1fafe071187
Valentindrap <valentindrap01@gmail.com>
2025-08-08 13:28:23 -0300

    Cambio en pagos

--- src/componentes/Inscripciones.js
+++ src/componentes/Inscripciones.js
@@ -43,16 +43,16 @@ function Inscripciones() {
       <ul>
         <li>
           <strong>Estudiantes universitarios de posgrado / Docentes y/o Investigadores:</strong><br />
-          Realizá el pago a UNIVERSIDAD NACIONAL DE LA PAM por $40.000,00 con el siguiente link:<br />
-          <a href="https://checkout.globalgetnet.com.ar/orders/ad8baa6f-c626-4700-8dbd-fbded464930f" target="_blank" rel="noopener noreferrer">
+          Realizá el pago a UNIVERSIDAD NACIONAL DE LA PAM por $70.000,00 con el siguiente link:<br />
+          <a href="https://checkout.globalgetnet.com.ar/orders/0874f659-afc4-4be9-8510-24813341781c" target="_blank" rel="noopener noreferrer">
           Haga click aqui para ir al enlace de pago
           </a>
         </li>
         <br />
         <li>
           <strong>Público en general:</strong><br />
-          Realizá el pago a UNIVERSIDAD NACIONAL DE LA PAM por $50.000,00 con el siguiente link:<br />
-          <a href="https://checkout.globalgetnet.com.ar/orders/ce24774e-c769-4df9-b96d-b9baa5ce7d9c" target="_blank" rel="noopener noreferrer">
+          Realizá el pago a UNIVERSIDAD NACIONAL DE LA PAM por $60.000,00 con el siguiente link:<br />
+          <a href="https://checkout.globalgetnet.com.ar/orders/5ea2f3f3-eba5-418a-acbc-8b2771eb3def" target="_blank" rel="noopener noreferrer">
             Haga click aqui para ir al enlace de pago
           </a>
         </li>
